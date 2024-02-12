98df86a0920c00c40170d5ad71f17a4704eaa780
William Delamare <william.delamare@acm.org>
2024-02-12 09:52:15 +0100

    [login] - corrected require_once in dbmodel

--- model/php/DBModel.php
+++ model/php/DBModel.php
@@ -31,7 +31,7 @@ class DBModel {
      * @return: true if the connection was successful, false otherwise
      */
     private function connect_to_db() {   
-        require_once __DIR__. "/env_settings.php";     
+        require __DIR__. "/env_settings.php";     
         try {
             $this->db = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $pwd, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
             return true;
